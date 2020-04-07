06187238c0a0bf8dc927e600552ca9f598eedd4d
Dong Won <donghyunwon610@gmail.com>
2020-04-06 23:50:36 -0400

    test

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -7,6 +7,28 @@ import os
 import time
 import config
 
+"""
+goal is to be able to dynamically search amazon and get prices for various items using scraping techniques
+"""
+
+#search and url
 search = config.search
 url = f"https://www.amazon.com/s?k={search}&ref=nb_sb_noss_2"
 
+#making requests using urllib
+
+req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+
+sauce = urllib.request.urlopen(req, timeout=5).read()
+
+code = urllib.request.urlopen(req, timeout=5).getcode()
+
+
+## check https code
+if code == 200:
+    print(f'connection success, http code : {code}')
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+else:
+    print(f'ran into error, http code : {code}')
+
+print(soup)
