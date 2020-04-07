13b6de72b2fdab8f0149e68bc3b7d6a375b9c27d
Dong Won <donghyunwon610@gmail.com>
2020-04-06 23:26:49 -0400

    first commit

---
+++ amazon_scrape.py
@@ -0,0 +1,11 @@
+
+import urllib.request
+import bs4 as bs
+import json
+import random
+import os
+import time
+import config
+
+search = config.search
+url = https://www.amazon.com/s?k=rtx+2080&ref=nb_sb_noss_2
